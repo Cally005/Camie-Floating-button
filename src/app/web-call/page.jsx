5df845831520e78e45b905cfe890d54ce5a8d865
Cally005 <chibuikemorafu@gmail.com>
2024-12-18 17:50:29 +0100

    removed an unwanted import

--- src/app/web-call/page.jsx
+++ src/app/web-call/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { startCall, endCall, useVapiEvents } from "./actions";
+
 
 const WebCall = () => {
   const [connecting, setConnecting] = useState(false);
